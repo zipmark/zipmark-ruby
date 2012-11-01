cf2666c08148864536758a1d01ae9c2ea4f97158
Jake Howerton <jake.howerton@gmail.com>
2012-11-01 14:21:37 -0600

    add some docs and make version have pre suffic

--- lib/zipmark.rb
+++ lib/zipmark.rb
@@ -11,6 +11,7 @@ require 'zipmark/version'
 
 require 'zipmark/adapters/httpclient_adapter'
 
+# Public: Main module for the Zipmark API Client
 module Zipmark
   # Public: URI String for the Zipmark Production API Endpoint.
   PRODUCTION_API_ENDPOINT = 'https://api.zipmark.com'
@@ -21,6 +22,9 @@ module Zipmark
   # Public: String Representing the Current Zipmark API Version
   API_VERSION = 'v2'
 
+  # Public: Error that is raised when a client is expected but not found
   class ClientError < StandardError; end
+
+  # Public: Error that is raised when a Resource is malformed or invalid
   class ResourceError < StandardError; end
 end
--- lib/zipmark/util.rb
+++ lib/zipmark/util.rb
@@ -1,5 +1,11 @@
 module Zipmark
+  # Public: Utility methods for the Zipmark API Client
   class Util
+    # Public: Method which converts all of a hash's keys to strings
+    #
+    # hash - The hash whose keys you would like to convert
+    #
+    # Returns the converted Hash
     def self.stringify_keys(hash)
       hash.inject({}) do |options, (key, value)|
         options[key.to_s] = value
