e41c18ead399ce72ac08e0802f22877499542e8b
Matt Chesler <matt@zipmark.com>
2013-08-09 10:51:04 -0400

    Remove unnecessary methods

--- lib/zipmark/link.rb
+++ lib/zipmark/link.rb
@@ -1,24 +1,12 @@
 module Zipmark
   class Link
-    attr_accessor :name, :title, :href
+    attr_accessor :rel, :title, :href
 
     def initialize(options)
       link = Util.stringify_keys(options)
       @rel = link["rel"]
       @title = link["title"]
       @href = link["href"]
-    end
-
-    def rel
-      @rel
-    end
-
-    def title
-      @title
-    end
-
-    def href
-      @href
     end
   end
 end
